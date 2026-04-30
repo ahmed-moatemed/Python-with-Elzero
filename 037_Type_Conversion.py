fc0f53c16641264356bb76779b99ad83360911e1
Ahmed Moatemed <matamedahmed@gmail.com>
2026-04-30 22:11:22 +0300

    Finish Two Vidoes 37,38 Talk About Type Conversion and input() Fun

---
+++ 037_Type_Conversion.py
@@ -0,0 +1,65 @@
+# ---------------------
+# -- Type Conversion --
+# ---------------------
+
+# str()
+
+a = 10.0
+print(type(a))
+print(type(str(a)))
+
+print("=" * 50)
+
+# tuple()
+
+c = "Ahmed" # String
+d = [1, 2, 3, 4, 5] # List
+e = {"A", "B", "C"} # Set
+f = {"A": 1, "B": 2} # Dictionary
+
+print(tuple(c))
+print(tuple(d))
+print(tuple(e))
+print(tuple(f))
+
+print("=" * 50)
+
+# list()
+
+c = "Ahmed" # String
+d = (1, 2, 3, 4, 5) # Tuple
+e = {"A", "B", "C"} # Set
+f = {"A": 1, "B": 2} # Dictionary
+
+print(list(c))
+print(list(d))
+print(list(e))
+print(list(f))
+
+print("=" * 50)
+
+# set()
+
+c = "Ahmed" # String
+d = (1, 2, 3, 4, 5) # Tuple
+e = ["A", "B", "C"] # List
+f = {"A": 1, "B": 2} # Dictionary
+
+print(set(c))
+print(set(d))
+print(set(e))
+print(set(f))
+
+print("=" * 50)
+
+# dict()
+
+c = "Ahmed" # String
+d = (("A", 1), ("B", 2), ("C", 3)) # Tuple
+e = [["One", 1], ["Two", 2], ["Three", 3]] # List
+f = {{"A", 1}, {"B", 2}} # Set
+
+# print(dict(c)) # Error
+print(dict(d))
+print(dict(e))
+# print(dict(f)) # Error
