562aa1c01f189dd78cd402eba5115455e2a86985
Ahmed Moatemed <matamedahmed@gmail.com>
2026-05-13 02:10:17 +0300

    first two vidoes in modules learn what is it and how to use and make

---
+++ 077_Modules_Part2_Create_Your_Module.py
@@ -0,0 +1,38 @@
+# -----------------------------------
+# -- Modules => Create Your Module --
+# -----------------------------------
+
+# import sys 
+# sys.path.append(r"D:\game")
+# print(sys.path)
+
+# import ahmed 
+# # print(dir(ahmed))
+
+# ahmed.sayHello("Ahmed")
+# ahmed.sayHello("Ali")
+# ahmed.sayHello("Sara")
+
+# ahmed.sayHowAreYou("Ahmed")
+# ahmed.sayHowAreYou("Ali")
+# ahmed.sayHowAreYou("Sara")
+
+# Alias
+# import ahmed as ah
+
+# ah.sayHello("Ahmed")
+# ah.sayHello("Ali")
+# ah.sayHello("Sara")
+
+# ah.sayHowAreYou("Ahmed")
+# ah.sayHowAreYou("Ali")
+# ah.sayHowAreYou("Sara")
+
+# from ahmed import sayHello
+
+# sayHello("Ahmed")
+
+from ahmed import sayHello as ss
+
+ss("Ahmed")
+
