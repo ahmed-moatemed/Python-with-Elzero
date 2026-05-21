c1d4cd468966421a8a7af5c5b470fb3daf513533
Ahmed Moatemed <matamedahmed@gmail.com>
2026-05-21 15:02:25 +0300

    Know how To Debugging and Type Hinting

---
+++ 093_Debugging_Code.py
@@ -0,0 +1,22 @@
+# --------------------
+# -- Debugging Code --
+# --------------------
+
+my_list = [1, 2, 3, 4, 5, 6]
+
+my_dict = {"Name": "Ahmed", "Age": 23, "Country": "Egypt"}
+
+for num in my_list :
+
+    print(num)
+
+for key, value in my_dict.items() :
+
+    print(f"{key} => {value}")
+
+def fun_one() :
+
+    print("Hello From Fun One")
+
+fun_one()
+
---
+++ 094_Type_Hinting.py
@@ -0,0 +1,16 @@
+# ------------------
+# -- Type Hinting --
+# ------------------
+
+
+def say_hello(name) -> str :  # Hint
+
+    print(f"Hello {name}")
+
+say_hello("Ahmed")
+
+def calc(x, y) -> int : # Write Type From Types You Studed
+
+    print(x + y)
+
+calc(10, 20)
