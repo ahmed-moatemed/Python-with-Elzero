6f3add48230feee2fcc8e36092a1cee81d88e769
Ahmed Moatemed <matamedahmed@gmail.com>
2026-05-30 17:57:59 +0300

    know what is regular expression and train about it

---
+++ 097_Regular_Expressions_Part3_Characters_Classes_Trainings.py
@@ -0,0 +1,12 @@
+# -------------------------------------------------------
+# -- Regular Expressions => Characters Classes Trainings --
+# -------------------------------------------------------
+# [0-9] select all numbers 0 to 9
+# [^0-9] select all except 0 to 9
+# [A-Z] select all capital charcters
+# [^A-Z] select all except capital charcters
+# [a-z] select all small charcters
+# [^a-z] select all except small charcters
+# ---------------
+
+# \s[A-Z]{2}\s[0-9]{3}\s\w{,6}
