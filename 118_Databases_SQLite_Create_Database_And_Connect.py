ea0fcf839fae79f51848c9b787bcc48f65b00454
Ahmed Moatemed <matamedahmed@gmail.com>
2026-07-14 01:40:30 +0300

    intro for Databases and create own database and connect it

---
+++ 118_Databases_SQLite_Create_Database_And_Connect.py
@@ -0,0 +1,19 @@
+# ------------------------------------------------------
+# -- Databases => SQLite => Create Database And Connect --
+# ------------------------------------------------------
+# - Connect 
+# - Execute 
+# - Close
+# -------------------------------------------------
+
+# Import SQLite Module
+import sqlite3
+
+# Create Database And Connect
+db = sqlite3.connect("app.db")
+
+# Create The Tables and Fields
+db.execute("CREATE TABLE IF NOT EXISTS skills (name TEXT, progress INTEGER, user_id INTEGER)")
+
+# Close Database 
+db.close()
