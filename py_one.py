3e7e71c22685873bfcac8540360ebff2b710990a
Ahmed Moatemed <matamedahmed@gmail.com>
2026-08-01 19:49:54 +0300

    first two lesson in advanced lesson

---
+++ py_one.py
@@ -0,0 +1,25 @@
+# File One
+
+print("Print From File One")
+
+if True:
+
+    print("True")
+
+else:
+
+    print("False")
+
+if __name__ == "__main__":
+
+    print("File One Is Running Directly")
+
+    def hello():
+
+        print("Print Function From File One")
+
+else:
+
+    print("You Are NOT Running File One Directly")
+
+hello()
---
+++ py_two.py
@@ -0,0 +1,5 @@
+# File Two
+
+import py_one
+
+# py_one.hello()
